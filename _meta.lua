02bb4c38c97d9b8a41c41c6a31245b727c1d6f0e
Andreas Lösel <andreas.loesel@outlook.com>
2025-11-24 18:06:46 +0100

    Update version in metadata

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = "weatherlockscreen",
     fullname = _("Weather Lockscreen"),
     description = _([[Displays weather information on the sleep screen. Select Weather option in Sleep screen > Wallpaper menu to enable.]]),
-    version = "v0.9.1-beta.2"
+    version = "v0.9.2-beta.1"
 }
