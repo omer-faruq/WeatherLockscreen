5b68d42a725fb2f5fec08a870c3014b4b3371f13
Andreas Lösel <andreas.loesel@outlook.com>
2025-12-29 21:12:26 +0100

    Release v0.9.5-beta.1

--- _meta.lua
+++ _meta.lua
@@ -2,6 +2,7 @@ local _ = require("l10n/gettext")
 return {
     name = "weatherlockscreen",
     fullname = _("Weather Lockscreen"),
-    description = _([[Displays weather information on the sleep screen. Select Weather option in Sleep screen > Wallpaper menu to enable.]]),
-    version = "v0.9.4-beta.1"
+    description = _(
+        [[Displays weather information on the sleep screen. Select Weather option in Sleep screen > Wallpaper menu to enable.]]),
+    version = "v0.9.5-beta.1"
 }
